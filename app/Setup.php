ca18e737b8f4d30412a25fa7e68f07dc6f0c32fe
gl-tfom <14111335+gl-tfom@users.noreply.github.com>
2020-03-17 12:46:36 -0700

    rf

--- app/Setup.php
+++ app/Setup.php
@@ -6,22 +6,25 @@ class Setup
 {
     public static function activation()
     {
-        if (!wp_next_scheduled('ddd')) {
-            wp_schedule_event(time(), 'every_minute', 'ddd');
+        if (!wp_next_scheduled('bb_email_reminder_cron')) {
+            wp_schedule_event(time(), 'every_minute', 'bb_email_reminder_cron');
         }
     }
 
     public static function deactivation()
     {
-        wp_clear_scheduled_hook('ddd');
+        wp_clear_scheduled_hook('bb_email_reminder_cron');
+    }
+
+    public static function cronSchedules()
+    {
+        add_filter('cron_schedules', function ($new_schedules) {
+            $new_schedules['every_minute'] = [
+                'interval' => 60,
+                'display'  => __('Every Minute'),
+            ];
+
+            return $new_schedules;
+        });
     }
 }
-
-// add_filter('cron_schedules', function ($new_schedules) {
-//     $new_schedules['every_minute'] = [
-//         'interval' => 60,
-//         'display'  => __('Every Minute'),
-//     ];
-
-//     return $new_schedules;
-// });
